44fdb681260a2ebd9628146fb03737cb0bdd5192
vitorcapdeville <vgcapdeville@hotmail.com>
2024-06-30 19:53:22 -0300

    ✨ Adicionar rota para trazer respostas a um determinado envio.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import Depends, FastAPI, HTTPException, Query, status
+from fastapi import Depends, FastAPI, HTTPException, status
 from sqlalchemy_utils import database_exists
 from sqlmodel import Session, select
 
@@ -20,3 +20,15 @@ def obter_respostas(envio_id: int, session: Annotated[Session, Depends(get_sessi
     if not respostas:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Respostas não encontradas.")
     return respostas
+
+
+@app.get("/respostas")
+def obter_respostas_psicologo(
+    email: str, session: Annotated[Session, Depends(get_session)]
+) -> list[QuestionariosEnviados]:
+    questionarios_enviados = session.exec(
+        select(QuestionariosEnviados).filter(QuestionariosEnviados.psicologo_email == email)
+    ).all()
+    if not questionarios_enviados:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Respostas não encontradas.")
+    return questionarios_enviados
