34083814e17fa44a728a60898fbd0f17f686b48f
vitorcapdeville <vgcapdeville@hotmail.com>
2024-07-06 19:37:08 -0300

    ✨ Criação de rotas para obter questionários enviados para um paciente e para gravar as respostas.

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,13 @@ from sqlalchemy_utils import database_exists
 from sqlmodel import Session, select
 
 from app.database import criar_db_e_tabelas, engine, get_session
-from app.models import QuestionariosBase, QuestionariosEnviados, QuestionarioPublic, QuestionariosEnviadosComStatus
+from app.models import (
+    QuestionariosBase,
+    QuestionariosEnviados,
+    QuestionarioPublic,
+    QuestionariosEnviadosComStatus,
+    Resposta,
+)
 
 if not database_exists(engine.url):
     criar_db_e_tabelas()
@@ -33,6 +39,7 @@ def obter_respostas_psicologo(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Respostas não encontradas.")
     return [
         QuestionariosEnviadosComStatus(
+            id=questionario.id,
             psicologo_email=questionario.psicologo_email,
             paciente_email=questionario.paciente_email,
             questionario_id=questionario.questionario_id,
@@ -42,7 +49,24 @@ def obter_respostas_psicologo(
     ]
 
 
+@app.get("/envios")
+def obter_envios_por_email(email: str, session: Annotated[Session, Depends(get_session)]) -> QuestionariosEnviados:
+    questionarios = session.exec(
+        select(QuestionariosEnviados).filter(QuestionariosEnviados.paciente_email == email)
+    ).all()
+    for questionario in questionarios:
+        if len(questionario.respostas) == 0:
+            return questionario
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Envios não encontrados.")
+
+
 @app.post("/envio")
 def registrar_envio(info_envio: QuestionariosBase, session: Annotated[Session, Depends(get_session)]) -> None:
     session.add(QuestionariosEnviados(**info_envio.model_dump()))
+    session.commit()
+
+
+@app.post("/responder")
+def gravar_respostas(info_respostas: list[Resposta], session: Annotated[Session, Depends(get_session)]) -> None:
+    session.add_all(info_respostas)
     session.commit()
--- app/models.py
+++ app/models.py
@@ -14,6 +14,7 @@ class QuestionariosEnviados(QuestionariosBase, table=True):
 
 
 class QuestionariosEnviadosComStatus(QuestionariosBase):
+    id: int
     respondido: bool
 
 
