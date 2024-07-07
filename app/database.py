b5b7633f4899b12a2b9e0c69367c85031588dceb
vitorcapdeville <vgcapdeville@hotmail.com>
2024-07-07 16:26:52 -0300

    🔧 Remover script de populacao da tabela, uma vez que agora é possível criar os dados via rotas e frontend.

--- app/database.py
+++ app/database.py
@@ -12,18 +12,6 @@ engine = create_engine(sqlite_url, connect_args=connect_args)
 def criar_db_e_tabelas():
     SQLModel.metadata.create_all(engine)
 
-    respostas = [
-        Resposta(envio_id=1, pergunta_id=1, alternativa_id=1),
-        Resposta(envio_id=1, pergunta_id=2, alternativa_id=3),
-        Resposta(envio_id=1, pergunta_id=5, alternativa_id=2),
-    ]
-    questionario = QuestionariosEnviados(
-        psicologo_email="vitor771@gmail.com", paciente_email="teste@email.com", questionario_id=1, respostas=respostas
-    )
-    with Session(engine) as session:
-        session.add(questionario)
-        session.commit()
-
 
 def get_session():
     with Session(engine) as session:
